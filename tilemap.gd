51e04706a88a138aa5c0781b03c9790456c07c9f
Matthew Madden <matthewkayin@gmail.com>
2024-02-01 06:55:08 -0800

    tile selection
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- tilemap.gd
+++ tilemap.gd
@@ -4,7 +4,11 @@ const LAYER_TILE = 0
 
 const NEIGHBOR_OFFSET = [Vector2i(0, -1), Vector2i(1, 0), Vector2i(0, 1), Vector2i(-1, 0)]
 
+@onready var selection = $selection
+
 var grass_tiles = {}
+var selecting = false
+var selected_tile = null
 
 func _ready():
     for i in range(-3, 3):
@@ -16,6 +20,7 @@ func _ready():
         if tile_data.get_custom_data("is_grass"):
             grass_tiles[tile_data.get_custom_data("moisture")][tile_data.get_custom_data("offset")] = atlas_coords
     update_tile_edges()
+    start_selecting()
 
 func update_tile_edges():
     for cell in get_used_cells(LAYER_TILE):
@@ -44,5 +49,25 @@ func update_tile_edges():
         var moisture = tile_data.get_custom_data("moisture")
         set_cell(LAYER_TILE, cell, tile_set.get_source_id(0), grass_tiles[moisture][offset])
 
-func _process(delta):
-    pass
+func start_selecting():
+    selecting = true
+    selected_tile = null
+
+func _process(_delta):
+    if selecting:
+        var mouse_position = get_global_mouse_position()
+        selected_tile = null
+        for cell in get_used_cells(LAYER_TILE):
+            var cell_center = map_to_local(cell)
+            var relative_mouse_position = mouse_position - cell_center
+            var mouse_inside_cell = (abs(relative_mouse_position.x) * 9.0) + (abs(relative_mouse_position.y) * 16.0) <= 9.0 * 16.0
+            if mouse_inside_cell:
+                selected_tile = cell
+                var cell_atlas_coords = get_cell_atlas_coords(LAYER_TILE, cell)
+                selection.region_rect.position = Vector2(cell_atlas_coords.x * 32, cell_atlas_coords.y * 16)
+                break
+    if selecting and selected_tile != null:
+        selection.position = map_to_local(selected_tile)
+        selection.visible = true
+    else:
+        selection.visible = false
